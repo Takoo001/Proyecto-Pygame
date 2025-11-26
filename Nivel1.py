b0688f19f99888391ee86a16372772db329da483
Joaquin Macias <omermacias@gmail.com>
2025-11-26 12:56:38 -0300

    Eliminando nivel 1 de prueba, reemplazandolo por el main de prueba

--- Nivel1.py
+++
@@ -1,6 +0,0 @@
-import pygame as pg
-from jugador import Jugador
-
-def nivel_1():
-    pg.init()
-    
---
+++ nivel1.py
@@ -0,0 +1,44 @@
+import pygame as pg
+
+from jugador import Jugador
+
+class Nivel1:
+    def __init__(self):
+        pass
+    
+    def iniciar(self):
+        pg.init()
+
+        background = (30, 30, 30)
+
+        ventana = pg.display.set_mode((640, 480))
+        pg.display.set_caption("Prueba")
+
+        jugador = Jugador(0, 300)
+
+        reloj = pg.time.Clock()
+
+        running = True
+        while running:
+
+            reloj.tick(60)
+            for event in pg.event.get():
+                if event.type == pg.QUIT: 
+                    running = False
+            ventana.fill(background)
+
+            teclas = pg.key.get_pressed()
+
+            posición_ant_jugador = jugador.obtener_posicion()
+
+            jugador.mover(teclas)
+
+            ventana.fill(background)
+
+            jugador.dibujar(ventana)
+
+            pg.display.update()
+
+        pg.quit
+
+Nivel1().iniciar()
