f322e1465cd86c9ac1301f632b54fdf601653e4e
Joaquin Macias <omermacias@gmail.com>
2025-11-29 06:44:20 -0300

    Configurando inicio de suelo (tamaño, posición)

--- suelo.py
+++ suelo.py
@@ -1,5 +1,19 @@
 import pygame as pg
+import nivel_settings as ns
 
-class Suelo():
-    def __init__(self):
-        pass
+class Suelo(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+        self.suelo = 1 #pg.image.load("assets\sprites_suelo\...").convert_alpha()
+        
+        # Posición Hitbox
+        self.x = x
+        self.y = y
+        self.rect = pg.Rect(self.x, self.y, 64, 64)
+
+    def dibujar(self, ventana):
+        pg.draw.rect(ventana, (255, 0, 0), self.rect, 1)
+
+    def obtener_posicion(self):
+        return (self.rect.x, self.rect.y)
