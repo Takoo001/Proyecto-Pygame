9d786bd620c843a2e1e4a1fc89ca24fd12f9b6e3
Joaquin Macias <omermacias@gmail.com>
2025-12-03 03:26:20 -0300

    Cambiando lógica de colisiones

--- nivel1.py
+++ nivel1.py
@@ -12,8 +12,8 @@ class Nivel1:
         pg.display.set_caption("Nivel1")
 
         # Estableciendo posición de inicio 
-        jugador = Jugador(0, ns.ALTO_NIVEL -64 - 50)
-        suelo = Suelo(0, ns.ALTO_NIVEL - 64)
+        jugador = Jugador(0, ns.ALTO_NIVEL -64 - 49)
+        suelo = Suelo()
 
         # Reloj para fps
         reloj = pg.time.Clock()
@@ -31,14 +31,17 @@ class Nivel1:
             
             teclas = pg.key.get_pressed()
 
+            posicion_jugador = jugador.obtener_posicion()
+
             jugador.movimiento(teclas)
             jugador.dibujar(ventana)
             
-            suelo.dibujar_suelo(suelo, ventana)
+            print(posicion_jugador)
+            for i in suelo.lista_suelos:
+                if jugador.hitbox.colliderect(i):
+                    jugador.restablecer_posicion((ns.ALTO_NIVEL -64 - 49))
 
-            posicion_jugador = jugador.obtener_posicion()
-            if jugador.hitbox.colliderect(suelo.rect):
-                jugador.restablecer_posicion(*posicion_jugador)
+            suelo.dibujar_suelo(ventana)
 
             pg.display.update()
 
