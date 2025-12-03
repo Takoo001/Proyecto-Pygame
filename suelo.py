117ad96ca44a638de1388fe09b53344495f7254a
Joaquin Macias <omermacias@gmail.com>
2025-12-03 03:26:54 -0300

    Implementando bucle para guardar sprites de suelos en una lista

--- suelo.py
+++ suelo.py
@@ -2,26 +2,16 @@ import pygame as pg
 import nivel_settings as ns
 
 class Suelo(pg.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self):
         super().__init__()
 
-        self.suelo = pg.image.load("assets\sprites_suelo\suelo_cesped.png").convert_alpha()
-        
-        # Posición Hitbox
-        self.x = x
-        self.y = y
-        self.rect = pg.Rect(self.x, self.y, 64, 64)
+        self.sprite_suelo = pg.image.load("assets\sprites_suelo\suelo_cesped.png")
+        self.lista_suelos = []
 
-    def dibujar(self, ventana):
-        ventana.blit(self.suelo, (self.x, self.y))
-        pg.draw.rect(ventana, (255, 0, 0), self.rect, 1)
+        for x in range(0, ns.ANCHO_NIVEL, 64):
+            self.lista_suelos.append(pg.Rect(x, ns.ALTO_NIVEL - 64, 64, 64))
 
-    def dibujar_suelo(self, suelo, ventana):
-        for sprite in range(0, ns.ANCHO_NIVEL, 64):
-            self.x = sprite
-            # self.rect.x es solo para cambiar también el eje x del dibujo de la "hitbox" roja de los sprites
-            self.rect.x = sprite
-            suelo.dibujar(ventana)
-
-    def obtener_posicion(self):
-        return (self.rect.x, self.rect.y)
+    def dibujar_suelo(self, ventana):
+        for suelo in self.lista_suelos:
+            ventana.blit(self.sprite_suelo, suelo.topleft)
+            #pg.draw.rect(ventana, (255, 0, 0), suelo, 1)
