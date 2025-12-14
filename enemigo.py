e941423c386811607fb5529ed3483d73b46ce471
Joaquin Macias <omermacias@gmail.com>
2025-12-14 03:03:32 -0300

    Agregando Lógica de seguimiento del enemigo al jugador

--- enemigo.py
+++ enemigo.py
@@ -12,7 +12,8 @@ class Enemigo(Entidad):
         self.estadisticas["Velocidad"] = 2
 
         # Cooldown Ataque
-        self.cooldown = 5000
+        self.cooldown = 3000
+        self.inicio_ataque = 1000 # Iniciar el ataque 1 segundo después de detectar el área de ataque con el jugador
 
         # Posición Hitbox
         self.hitbox = pg.Rect(self.x, self.y, 64, 64)
@@ -30,12 +31,39 @@ class Enemigo(Entidad):
             self.imagen_ataque = self.frames_ataque[0]
 
     def movimiento(self, jugador):
-        if self.hitbox.x > jugador.hitbox.x:
+        tiempo_actual = pg.time.get_ticks()
+
+        if self.hitbox.x > jugador.hitbox.x - 40:
             self.rect.x -= 1 * self.estadisticas["Velocidad"]
-        if self.hitbox.x < jugador.hitbox.x:
+            self.direccion = "IZQUIERDA"
+            self.flip = True
+            if self.ataque_hitbox.colliderect(jugador.hitbox):
+                if tiempo_actual - self.ultimo_ataque > self.cooldown and tiempo_actual - self.inicio_ataque:
+                    if not self.atacando:
+                        self.atacando = True
+                        self.ultimo_ataque = tiempo_actual
+                        self.inicio_ataque = tiempo_actual
+
+        if self.hitbox.x < jugador.hitbox.x + 40:
             self.rect.x += 1 * self.estadisticas["Velocidad"]
-        
+            self.direccion = "DERECHA"
+            self.flip = False
+            if self.ataque_hitbox.colliderect(jugador.hitbox):
+                if tiempo_actual - self.ultimo_ataque > self.cooldown and tiempo_actual - self.inicio_ataque:
+                    if not self.atacando:
+                        self.atacando = True
+                        self.ultimo_ataque = tiempo_actual
+                        self.inicio_ataque = tiempo_actual
+
         self.hitbox.center = self.rect.center
+
+        if self.direccion == "DERECHA":
+            self.ataque_hitbox = pg.Rect(self.rect.centerx + 15, self.rect.centery - 48, 64, 64)
+        if self.direccion == "IZQUIERDA":
+            self.ataque_hitbox = pg.Rect(self.rect.centerx - 79, self.rect.centery - 48, 64, 64)
+
+        if self.atacando:
+            self.animar_ataque()
 
     def dibujar(self, ventana):
         if self.atacando:
@@ -46,7 +74,7 @@ class Enemigo(Entidad):
         imagen_flip = pg.transform.flip(self.imagen, flip_x= self.flip, flip_y= False)
         ventana.blit(imagen_flip, self.rect.topleft)
         pg.draw.rect(ventana, (255, 0, 0), self.hitbox, 1)
-
+    
 class EnemigoPequeno(Enemigo):
     def __init__(self, x, y):
         super().__init__(x, y)
@@ -54,3 +82,4 @@ class EnemigoPequeno(Enemigo):
         # Sprites
         self.sprite_quieto = pg.image.load("assets\sprites_enemigos\enemigo_prueba.png").convert_alpha()
         self.imagen = self.sprite_quieto
+        self.direccion = "IZQUIERDA"
