cd0dc61dbb790f58ec7fcc77f64f87830e560cc3
Joaquin Macias <omermacias@gmail.com>
2025-12-16 00:45:49 -0300

    Creando más suelos

--- suelo.py
+++ suelo.py
@@ -8,10 +8,10 @@ class Suelo(pg.sprite.Sprite):
         self.sprite_suelo = pg.image.load("assets\sprites_suelo\suelo_cesped.png")
         self.lista_suelos = []
 
-        for x in range(0, ns.ANCHO_NIVEL * 2, 64):
+        for x in range(0, ns.ANCHO_NIVEL * 4, 64):
             self.lista_suelos.append(pg.Rect(x, ns.ALTO_NIVEL - 64, 64, 64))
-
     def dibujar_suelo(self, ventana, mundo_x):
+
         for suelo in self.lista_suelos:
             ventana.blit(self.sprite_suelo, (suelo.topleft[0] + mundo_x, suelo.topleft[1]))
             #pg.draw.rect(ventana, (255, 0, 0), suelo, 1)
