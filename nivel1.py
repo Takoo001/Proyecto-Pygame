02767c83ee6c9da052b6d88ee1d4a5b463002e9e
Joaquin Macias <omermacias@gmail.com>
2025-12-15 21:32:12 -0300

    Probando a crear camara que siga al jugador

--- nivel1.py
+++ nivel1.py
@@ -26,18 +26,14 @@ class Nivel1:
         fondo_ancho = 2560
         fondo_alto = 480
         fondo = pg.Surface((fondo_ancho, fondo_alto))
+        mundo_x = 0
         
         for i in range(fondo_ancho // ns.ANCHO_NIVEL):
             sprite_fondo = pg.image.load("assets\\sprites_fondo\\fondo_prueba.png").convert()
             fondo.blit(sprite_fondo, (i * 640, 0))
 
-        camara = pg.Rect(0, 0, ns.ANCHO_NIVEL, ns.ALTO_NIVEL)
-
         # Reloj para fps
         reloj = pg.time.Clock()
-
-        ESCALA_X = 1
-        ESCALA_Y = 1
 
         # Bucle para mantener abierto el nivel
         running = True
@@ -53,18 +49,24 @@ class Nivel1:
                 ventana = pg.display.set_mode((ns.ANCHO_NIVEL, ns.ALTO_NIVEL), pg.RESIZABLE)
 
             ventana.fill(ns.BACKGROUND)
-
             teclas = pg.key.get_pressed()
 
+            ventana.blit(fondo, (mundo_x, 0))
+            mundo_x = -jugador.hitbox.centerx
             # Métodos Jugador
             jugador.movimiento(teclas)
-            jugador.dibujar(ventana)
+            jugador.dibujar(ventana, mundo_x)
             #print(jugador.hitbox.center)
+
+            if jugador.rect.left < 0:
+                jugador.rect.left = 0
+            if jugador.rect.right > ns.ANCHO_NIVEL:
+                jugador.rect.right = ns.ANCHO_NIVEL
 
             # Métodos Enemigos
             for enemigo in enemigos_pequenos:
                 if enemigo.vivo:
-                    enemigo.dibujar(ventana)
+                    enemigo.dibujar(ventana, mundo_x)
                     enemigo.movimiento(jugador)
 
                     # Sistema colisión ataque jugador con enemigo
@@ -78,7 +80,9 @@ class Nivel1:
                             if enemigo.estadisticas["Vida"] <= 0:
                                 print("Enemigo Muerto")
                                 enemigo.vivo = False
-                                jugador.estadisticas["Vida"] += 5
+                                jugador.estadisticas["Vida"] += 10
+                                if jugador.estadisticas["Vida"] > 100:
+                                    jugador.estadisticas = 100
                             jugador.tick_ataque = True
 
                     # Sistema colisión ataque enemigo con jugador
@@ -90,21 +94,15 @@ class Nivel1:
                                 enemigo.tick_ataque = True
                 else:
                     enemigo.muerto()
+                    enemigos_pequenos.remove(enemigo)
 
             # Métodos Suelo
-            suelo.dibujar_suelo(ventana)
-
-            if jugador.hitbox.right > camara.right - 50:
-                camara.x = jugador.hitbox.right - ns.ANCHO_NIVEL + 50
-            if jugador.hitbox.left < camara.left + 50:
-                camara.x = jugador.hitbox.left - 50
-
-            camara.x = max(0, min(camara.x, fondo_ancho - ns.ANCHO_NIVEL))
+            suelo.dibujar_suelo(ventana, mundo_x)
 
             for i in suelo.lista_suelos:
                 if jugador.hitbox.colliderect(i):
                     jugador.restablecer_posicion(ns.ALTO_NIVEL -64 - 49)
-                        
+            
             pg.display.update()
 
         pg.quit()
