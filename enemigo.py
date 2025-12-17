1f33f5d28e657d2898a3e2ebe2ec05c3b738dac6
Joaquin Macias <omermacias@gmail.com>
2025-12-17 04:41:10 -0300

    Agregando sprites propios

--- enemigo.py
+++ enemigo.py
@@ -67,11 +67,14 @@ class EnemigoPequeno(Enemigo):
         super().__init__(x, y)
 
         # Sprites de Enemigo Pequeño
-        self.sprite_quieto = pg.image.load(
-            "assets/sprites_enemigos/español_pequeño_quieto.png"
-        ).convert_alpha()
-        self.sprite_corriendo = pg.image.load(
-            "assets/sprites_enemigos/español_pequeño_corriendo.png"
-        ).convert_alpha()
+        self.sprite_quieto = pg.image.load("assets/sprites_enemigos/español_pequeño_quieto.png").convert_alpha()
+        self.sprite_corriendo = pg.image.load("assets/sprites_enemigos/español_pequeño_corriendo.png").convert_alpha()
+        self.sprite_ataque = pg.image.load("assets/sprites_enemigos/ataque_español_pequeño.png").convert_alpha()
 
+        # Sprites
         self.frames_corriendo = self.recortar_frames(self.sprite_corriendo, 8, 64, 64)
+        self.frames_ataque = self.recortar_frames(self.sprite_ataque, 12, 64, 64)
+
+        # Modificando cooldown de ataque y rapidez de frames
+        self.cooldown = 2000
+        self.frame_milisegundos_ataque = 50
