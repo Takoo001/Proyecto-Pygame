a60a0fa367a019cd077af8c1fde89d693206d6b1
Joaquin Macias <omermacias@gmail.com>
2025-12-17 03:36:58 -0300

    Añadiendo sprites nuevos

--- enemigo.py
+++ enemigo.py
@@ -27,13 +27,15 @@ class Enemigo(Entidad):
     def movimiento(self, jugador):
         tiempo_actual = pg.time.get_ticks()
 
+        mov_x = 0
+
         if self.hitbox.centerx > jugador.hitbox.centerx + 60:
-            self.rect.x -= self.velocidad
+            mov_x -= self.velocidad
             self.direccion = "IZQUIERDA"
             self.flip = True
 
         elif self.hitbox.centerx < jugador.hitbox.centerx - 60:
-            self.rect.x += self.velocidad
+            mov_x += self.velocidad
             self.direccion = "DERECHA"
             self.flip = False
 
@@ -50,18 +52,26 @@ class Enemigo(Entidad):
                 self.ultimo_ataque = tiempo_actual
                 self.atacando = True
 
+        self.rect.x += mov_x
+
         if self.atacando:
             self.animar_ataque()
 
+        if mov_x != 0:
+            self.animar_corriendo()
+        else:
+            self.imagen = self.sprite_quieto
 
 class EnemigoPequeno(Enemigo):
     def __init__(self, x, y):
         super().__init__(x, y)
 
+        # Sprites de Enemigo Pequeño
         self.sprite_quieto = pg.image.load(
             "assets/sprites_enemigos/español_pequeño_quieto.png"
         ).convert_alpha()
-        self.imagen = self.sprite_quieto
         self.sprite_corriendo = pg.image.load(
             "assets/sprites_enemigos/español_pequeño_corriendo.png"
         ).convert_alpha()
+
+        self.frames_corriendo = self.recortar_frames(self.sprite_corriendo, 8, 64, 64)
